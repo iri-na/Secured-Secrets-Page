ae6b768c4498194145249ca45302af5d6d701479
Irina <irina@Irinas-MacBook-Air.local>
2020-05-08 05:41:33 -0400

    Secure with bcrypt Salting and Hashing

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -29,16 +30,18 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, function(err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, match) {
+                    if (match) {
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
@@ -49,17 +52,23 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    const username = req.body.username;
+    const password = req.body.password;
+
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: username,
+            password: hash
+        });
+        newUser.save(function(err) {
+            if (!err) {
+                res.render("secrets");
+            } else {
+                console.log(err);
+            }
+        });
     });
-    newUser.save(function(err) {
-        if (!err) {
-            res.render("secrets");
-        } else {
-            console.log(err);
-        }
-    });
+
 });
 
 app.listen(3000, function() {
